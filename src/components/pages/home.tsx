f2c0b521195d0a2c11f619d9a23ff6f3c72bc63d
Dakota Shapiro <dakotashapiro15@gmail.com>
2024-05-02 18:11:24 -0400

    minor change

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -4,20 +4,20 @@ import { TableView } from "../views";
 import { FlexColumn, Menu, MenuItem } from "..";
 
 export const Home = () => {
-    const fileState = useAppSelector((state) => state.file);
+  const fileState = useAppSelector((state) => state.file);
 
-    console.log(fileState.keys);
-    console.log(fileState.objects);
-    return (
-        <FlexColumn alignItems="center">
-            <Typography variant="h4" textAlign="left">
-                {fileState.name}
-            </Typography>
-            <Menu>
-                <MenuItem title="Table">
-                    <TableView state={fileState} />
-                </MenuItem>
-            </Menu>
-        </FlexColumn>
-    );
+  console.log(fileState.keys);
+  console.log(fileState.objects);
+  return (
+    <FlexColumn alignItems="center">
+      <Typography variant="h4" textAlign="left">
+        {fileState.name}
+      </Typography>
+      <Menu>
+        <MenuItem title="Table">
+          <TableView state={fileState} height="50em" />
+        </MenuItem>
+      </Menu>
+    </FlexColumn>
+  );
 };
--- src/components/views/table-view.tsx
+++ src/components/views/table-view.tsx
@@ -1,50 +1,50 @@
 import { FileState } from "../../util";
 import {
-    Paper,
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
-    TableRow,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
 } from "@mui/material";
 
 export const TableView = ({
-    state,
-    height = "100%",
-    width = "100%",
+  state,
+  height = "100%",
+  width = "100%",
 }: {
-    state: FileState;
-    width?: string;
-    height?: string;
+  state: FileState;
+  width?: string;
+  height?: string;
 }) => {
-    return (
-        <TableContainer
-            component={Paper}
-            sx={{ height, width, overflow: "scroll" }}
-        >
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        {state.keys.map((key, index) => (
-                            <TableCell align="left" key={index}>
-                                {key}
-                            </TableCell>
-                        ))}
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {state.objects.map((rec, index) => (
-                        <TableRow key={index}>
-                            {state.keys.map((key, index) => (
-                                <TableCell align="left" key={`cell-${index}`}>
-                                    {rec[key]}
-                                </TableCell>
-                            ))}
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
-    );
+  return (
+    <TableContainer
+      component={Paper}
+      sx={{ height, width, overflow: "scroll" }}
+    >
+      <Table>
+        <TableHead>
+          <TableRow>
+            {state.keys.map((key, index) => (
+              <TableCell align="left" key={index}>
+                {key}
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {state.objects.map((rec, index) => (
+            <TableRow key={index}>
+              {state.keys.map((key, index) => (
+                <TableCell align="left" key={`cell-${index}`}>
+                  {rec[key]}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 };
